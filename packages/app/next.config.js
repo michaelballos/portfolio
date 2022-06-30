a16603542288d8dccedb6457951105d299e674b6
michaelballos <ballos.michael@gmail.com>
2022-06-30 02:45:57 -0500

    Resolve font

--- packages/app/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    GA_TRACKING_ID: process.env.GA_TRACKING_ID
-  }
-}
-
-module.exports = nextConfig
